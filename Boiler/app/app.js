387d17563f0bdff2d8e4d01410324e74d38e8cd8
mikevh <mikevh@gmail.com>
2016-02-04 07:39:18 -0800

    select text on focus

--- Boiler/app/app.js
+++ Boiler/app/app.js
@@ -61,9 +61,18 @@
         $httpProvider.interceptors.push('AuthorizationRedirectInterceptor');
     });
 
-    app.controller('indexController', function ($scope, $location) {
-        $scope.go = function (dest) {
-            $location.path(dest);
+    app.directive('selectOnClick', ['$window', function ($window) {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                element.on('click', function () {
+                    if (!$window.getSelection().toString()) {
+                        // Required for mobile Safari
+                        this.setSelectionRange(0, this.value.length);
+                    }
+                });
+            }
         };
-    });
+    }]);
+
 })();
---
+++ Boiler/app/index.js
@@ -0,0 +1,12 @@
+﻿(function() {
+    'use strict';
+
+    var app = angular.module('app');
+
+    app.controller('indexController', function ($scope, $location, $window) {
+        $scope.go = function (dest) {
+            $location.path(dest);
+        };
+    });
+
+})();
